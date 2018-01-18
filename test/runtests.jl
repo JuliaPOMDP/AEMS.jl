785912d13d73a2e3c99d7847899839cc579ecde1
dressel <louisdressel@gmail.com>
2018-01-18 13:58:59 -0800

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,45 @@
 using AEMS
+using POMDPs
+using POMDPToolbox
+using POMDPModels
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+#@requirements_info solver pomdp
+
+######################################################################
+# BABY POMDP Tests
+######################################################################
+pomdp = BabyPOMDP()
+up = DiscreteUpdater(pomdp)
+
+# testing solver with :clear root_manager
+solver = AEMSSolver(n_iterations = 1000, max_time = .1, updater = up)
+test_solver(solver, pomdp)
+
+# testing solver with :belief root_manager
+solver = AEMSSolver(n_iterations=1000, max_time=.1, updater=up, rm=:belief)
+test_solver(solver, pomdp)
+
+# testing solver with :user root_manager
+solver = AEMSSolver(n_iterations=1000, max_time=.1, updater=up, rm=:user)
+test_solver(solver, pomdp)
+
+
+
+######################################################################
+# TIGER PROBLEM TESTS
+######################################################################
+pomdp = TigerPOMDP()
+up = DiscreteUpdater(pomdp)
+
+# testing solver with :clear root_manager
+solver = AEMSSolver(n_iterations = 1000, max_time=.1, updater=up)
+test_solver(solver, pomdp)
+
+# testing solver with :belief root_manager
+solver = AEMSSolver(n_iterations = 1000, max_time=.1,updater=up,rm=:belief)
+test_solver(solver, pomdp)
+
+# testing solver with :belief root_manager
+solver = AEMSSolver(n_iterations = 1000, max_time=.1, updater=up, rm=:user)
+test_solver(solver, pomdp)
